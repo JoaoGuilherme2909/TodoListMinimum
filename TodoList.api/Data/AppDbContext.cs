225277fd281d4aedbd294009d44c32f857216d9d
joao <joaoguilhermesantos2909@gmail.com>
2024-10-14 15:58:28 -0300

    Add project files.

---
+++ TodoList.api/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoList.api.Models;
+
+namespace TodoList.api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> opts) : base(opts) {}
+
+    public DbSet<Todo> Todos { get; set; }
+}
---
+++ TodoList.api/Models/Todo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.api.Models;
+
+public class Todo
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Description { get; set; }
+    [Required]
+    public bool Completed { get; set; } = false;
+    [Required]
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime? UpdatedAt { get; set; } = null;
+}
